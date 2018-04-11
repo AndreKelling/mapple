f288c4702ef6b70d02eb17c70435c74ca69ff3c2
André J. Kelling <kontakt@andrekelling.de>
2018-04-11 12:07:01 +0200

    REMOVE admin unused css and js

--- admin/class-mapple-admin.php
+++ admin/class-mapple-admin.php
@@ -91,52 +91,6 @@ class Mapple_Admin {
 	} // add_menu()
 
 	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Mapple_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Mapple_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/mapple-admin.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Mapple_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Mapple_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/mapple-admin.js', array( 'jquery' ), $this->version, false );
-
-	}
-
-	/**
 	 * Creates a new custom post type
 	 *
 	 * @since 	1.0.0
