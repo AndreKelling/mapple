281a168eaa2d4bb80336d5dcd9d6291e28b03c42
André J. Kelling <kontakt@andrekelling.de>
2018-04-11 11:33:17 +0200

    L10N add working translations

--- admin/partials/mapple-admin-metabox-client-additional-info.php
+++ admin/partials/mapple-admin-metabox-client-additional-info.php
@@ -14,9 +14,9 @@ wp_nonce_field( $this->plugin_name, 'client_additional_info' );
 
 $atts 					= array();
 $atts['class'] 			= 'widefat';
-$atts['description'] 	= 'enter Address will autocomplete and fill geolocation field below. Change this field to fit your language. Google maps API should be fine with.';
+$atts['description'] 	= __('enter Address will autocomplete and fill geolocation field below. Change this field to fit your language. Google maps API should be fine with.', 'mapple' );
 $atts['id'] 			= 'client-address';
-$atts['label'] 			= 'Address';
+$atts['label'] 			= __('Address', 'mapple' );
 $atts['name'] 			= 'client-address';
 $atts['placeholder'] 	= '';
 $atts['type'] 			= 'text';
@@ -38,9 +38,9 @@ include( plugin_dir_path( __FILE__ ) . $this->plugin_name . '-admin-field-text-a
 
 $atts 					= array();
 $atts['class'] 			= 'widefat';
-$atts['description'] 	= 'enter Geo data like "52.457072, 13.510904". If not filled client won\'t appear on map!';
+$atts['description'] 	= __('enter Geo data like "52.457072, 13.510904". If not filled client won\'t appear on map!', 'mapple' );
 $atts['id'] 			= 'client-location';
-$atts['label'] 			= 'Location';
+$atts['label'] 			= __('Location', 'mapple' );
 $atts['name'] 			= 'client-location';
 $atts['placeholder'] 	= '52.457072, 13.510904';
 $atts['type'] 			= 'text';
@@ -62,9 +62,9 @@ include( plugin_dir_path( __FILE__ ) . $this->plugin_name . '-admin-field-text.p
 
 $atts 					= array();
 $atts['class'] 			= 'widefat';
-$atts['description'] 	= 'enter full URL with http:// or https://';
+$atts['description'] 	= __('enter full URL with http:// or https://', 'mapple' );
 $atts['id'] 			= 'client-url';
-$atts['label'] 			= 'Website URL';
+$atts['label'] 			= __('Website URL', 'mapple' );
 $atts['name'] 			= 'client-url';
 $atts['placeholder'] 	= 'https://andrekelling.de';
 $atts['type'] 			= 'text';
@@ -84,9 +84,9 @@ include( plugin_dir_path( __FILE__ ) . $this->plugin_name . '-admin-field-text.p
 
 $atts 					= array();
 $atts['class'] 			= 'widefat';
-$atts['description'] 	= 'show a different URL title';
+$atts['description'] 	= __('show a different URL title', 'mapple' );
 $atts['id'] 			= 'client-urlname';
-$atts['label'] 			= 'Website URL shown name';
+$atts['label'] 			= __('Website URL shown name', 'mapple' );
 $atts['name'] 			= 'client-urlname';
 $atts['placeholder'] 	= 'andrekelling.de';
 $atts['type'] 			= 'text';
